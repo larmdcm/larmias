5ae74cce97ea269cdd461603bee6127b25c69683
larmdcm <mdcm98@163.com>
2023-02-16 23:49:52 +0800

    update framework function

--- src/framework/src/functions.php
+++ src/framework/src/functions.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 namespace Larmias\Framework;
 
 use Larmias\Contracts\DotEnvInterface;
-use Larmias\Contracts\LoggerInterface;
 use Larmias\Contracts\StdoutLoggerInterface;
-use Larmias\Contracts\TranslatorInterface;
 use Larmias\Di\Container;
 use Larmias\Contracts\ApplicationInterface;
 use Larmias\Contracts\ConfigInterface;
-use Psr\Log\LogLevel;
 
 /**
  * make
@@ -82,45 +79,4 @@ function console(): StdoutLoggerInterface
     /** @var StdoutLoggerInterface $console */
     $console = make(StdoutLoggerInterface::class);
     return $console;
-}
-
-/**
- * 翻译
- *
- * @param string $key
- * @param array $vars
- * @param string|null $locale
- * @return string
- */
-function trans(string $key, array $vars = [], ?string $locale = null): string
-{
-    /** @var TranslatorInterface $translator */
-    $translator = make(TranslatorInterface::class);
-    return $translator->trans($key, $vars, $locale);
-}
-
-/**
- * 获取日志操作对象
- *
- * @return LoggerInterface
- */
-function logger(): LoggerInterface
-{
-    /** @var LoggerInterface $logger */
-    $logger = make(LoggerInterface::class);
-    return $logger;
-}
-
-/**
- * 日志记录.
- *
- * @param mixed $message
- * @param string $level
- * @param array $context
- * @return void
- */
-function trace(mixed $message, string $level = LogLevel::INFO, array $context = []): void
-{
-    $logger = logger();
-    $logger->log($level, \is_scalar($message) ? $message : (string)\var_export($message, true), $context);
 }
